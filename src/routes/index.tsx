fdc0949f5f24e6b7f6d309814577b90b18877708
PatrickJS <github@patrickjs.com>
2024-03-31 18:24:11 -0700

    refactor: use jsr package

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,21 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import pjs from "@jsr/patrickjs__test-package";
 
 export default component$(() => {
+  const message = useSignal(pjs(" I'm from the Server"));
+  // eslint-disable-next-line qwik/no-use-visible-task
+  useVisibleTask$(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    message.value = pjs(" I'm visible on the Client");
+  });
   return (
     <>
       <h1>Hi 👋</h1>
       <p>
         Can't wait to see what you build with qwik!
         <br />
-        Happy coding.
+        {message.value}
       </p>
     </>
   );
